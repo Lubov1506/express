f1f0a83aceab1c42fa34a233db6b07df66c8525e
Lubov Parfyonova <lubaparfyonova@gmail.com>
2022-10-11 12:16:04 +0300

    updateUser

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const {validate: validateBody} = require('./middlewares/validate.mw')
 const UserController = require('./controllers/user.controller')
 
 app.get('/users', UserController.getAllUsers)
+app.get('/user/:id', UserController.getSingleUser)
 app.post('/', bodyParser, validateBody, UserController.createUser)
+app.put('/user/:id', bodyParser, validateBody, UserController.updateUser)
 app.delete('/user/:id', UserController.deleteUser)
 
 app.listen(PORT, () => {
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -5,13 +5,31 @@ module.exports.createUser = async (req, res, next) => {
   const user = await new User(validateBody)
   res.status(201).send(user)
 }
+module.exports.updateUser = async (req, res, next) => {
+  const {
+    body: validateBody,
+    params: { id }
+  } = req
+  const user = await User.findOne(id)
+  const newUser = await user.update(validateBody)
+  res.status(202).send(newUser)
+}
+module.exports.getSingleUser = async (req, res, next) => {
+  const {
+    params: { id }
+  } = req
+  const user = await User.findOne(id)
+  res.status(200).send(user )
+}
 module.exports.getAllUsers = async (req, res, next) => {
-    const users = await User.findAll()
-    res.status(200).send(users)
+  const users = await User.findAll()
+  res.status(200).send(users)
 }
 module.exports.deleteUser = async (req, res, next) => {
-    const {params: {id}} = req
-    const user = await User.findOne(id)
-    const verdict = await user.delete()
-    res.status(200).send(verdict)
+  const {
+    params: { id }
+  } = req
+  const user = await User.findOne(id)
+  const verdict = await user.delete()
+  res.status(200).send(verdict)
 }
