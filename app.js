4c8c3a4df21d73169d0cc4a18efc7cffe407c3eb
Lubov Parfyonova <lubaparfyonova@gmail.com>
2022-10-11 10:38:09 +0300

    createUser app

--- app.js
+++ app.js
@@ -1,49 +1,16 @@
 const express = require('express')
-const yup = require('yup')
+
 const app = express()
-
 const PORT = 3000
-
+const {validate: validateBody} = require('./middlewares/validate.mw')
+const {createUser} = require('./controllers/user.controller')
 const bodyParser = express.json()
-const users = []
-app.post('/', bodyParser, async (req, res, next) => {
-  const { body } = req
-  const validateSchema = yup.object({
-    name: yup.string().required(),
-    email: yup
-      .string()
-      .email()
-      .required(),
-    password: yup.string().required(),
-    gender: yup.string().required(),
-    isSubscribe: yup.string()
-  })
-  try {
-    const validatedBody = await validateSchema.validate(body)
-    validatedBody.id = users.length
-    validatedBody.createdAt = new Date()
-    delete validatedBody.password
-    users.push(validatedBody)
-    res.send(users)
-  } catch (error) {
-    res.send(error.message)
-  }
-})
+
+app.post('/', bodyParser, validateBody, createUser)
 
 app.listen(PORT, () => {
   console.log(`server started on ${PORT} port`)
 })
 
-/* const express = require('express')
-const PORT = 3000
-const app = express()
 
 
-
-
-app.listen(PORT, () => {
-  console.log(`sever started on ${PORT}`)
-})
-app.get('/', (req, res)=>{
-res.send('hello from express')
-}) */
---
+++ middlewares/validate.mw.js
@@ -0,0 +1,22 @@
+const yup = require('yup')
+const validateSchema = yup.object({
+  name: yup.string().required(),
+  email: yup
+    .string()
+    .email()
+    .required(),
+  password: yup.string().required(),
+  gender: yup.string().required(),
+  isSubscribe: yup.string()
+})
+
+module.exports.validate = async (req, res, next) => {
+  const { body } = req
+
+  try {
+    req.body = await validateSchema.validate(body)
+    next()
+  } catch (error) {
+    res.send(error.message)
+  }
+}
